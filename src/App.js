d39cbbe24bce2f622858bb5d3f8f8ebb81d6dfcd
Mateusz Surdykowski <mateusz.surdykowski@gmail.com>
2022-06-13 14:35:07 +0200

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import GlobalStyles from "./components/styles/Global";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
 	return (
-		<>
+		<Router>
 			<GlobalStyles />
-		</>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="*" element={<ErrorPage />} />
+			</Routes>
+		</Router>
 	);
 }
 
